581720b0b63264cd391aa4bf2f44302ad90d6213
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-07-01 11:36:22 -0300

    CSS improvements and clean form

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -18,10 +18,12 @@ export default class Todo extends Component {
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
         this.handleSearch = this.handleSearch.bind(this)
+        this.handleClear = this.handleClear.bind(this)
 
         this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
-        this.handleMarkAsPending= this.handleMarkAsPending
+        this.handleMarkAsPending= this.handleMarkAsPending.bind(this)
         this.handleRemove = this.handleRemove.bind(this)
+        this.handleSearch = this.handleSearch.bind(this)
 
         this.refresh()
     }
@@ -67,6 +69,11 @@ export default class Todo extends Component {
         .then(resp => this.refresh(this.state.description))
     }
 
+    handleClear(){
+
+        this.refresh()
+    }
+
     render() {
 
         return (
@@ -76,7 +83,9 @@ export default class Todo extends Component {
                     description={this.state.description}
                     handleChange={this.handleChange}
                     handleAdd={this.handleAdd} 
-                    handleSearch={this.handleSearch}/>
+                    handleSearch={this.handleSearch}
+                    handleClear={this.handleClear}/>
+                    
                 <TodoList list={this.state.list}
                 handleMarkAsDone={this.handleMarkAsDone}
                 handleMarkAsPending={this.handleMarkAsPending}
