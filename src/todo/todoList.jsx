06aa88c15eba49a18cb171a5b33a1e69bb225732
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-07-01 10:20:20 -0300

    query and delete all

--- src/todo/todoList.jsx
+++ src/todo/todoList.jsx
@@ -1,8 +1,31 @@
 import React from "react";
+import IconButton from "../template/iconButton";
 
-export default props => (
+const TodoList = props => {
+  const renderRows = () => {
+      const list = props.list || []
+      return list.map(todo => (
+          <tr key={todo._id}>
+              <td>{todo.description}</td>
+              <td>
+                  <IconButton btnStyle='danger' icon='trash-o' onClick={() => props.handleRemove(todo)}/>
+              </td>
+          </tr>
+      )) 
+  }
+  return (
+      <table className='table'>
+          <thead>
+              <tr>
+                  <th>Descrição</th>
+                  <th>Ações</th>
+              </tr>
+          </thead>
+          <tbody>
+              {renderRows()}
+          </tbody>
+      </table>
+  )
+}
 
-    <div>
-      <h1>List</h1>
-    </div>
-)
+export default TodoList
