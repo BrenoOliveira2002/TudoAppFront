b2c26e00c144ec2bfc616c8c6b8777cc57062600
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-07-01 10:51:10 -0300

    mark as completed

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -18,6 +18,8 @@ export default class Todo extends Component {
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
 
+        this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
+        this.handleMarkAsPending= this.handleMarkAsPending
         this.handleRemove = this.handleRemove.bind(this)
 
         this.refresh()
@@ -46,6 +48,16 @@ export default class Todo extends Component {
         .then(resp => this.refresh())
 
     }
+    handleMarkAsDone(todo) {
+        axios.put(`${URL}/${todo.id}`, {...todo, done: true })
+        .then(resp => this.refresh())
+    }
+
+    handleMarkAsPending(todo) {
+
+        axios.put(`${URL}/${todo._id}`, {...todo, done: false})
+        .then(resp => this.refresh())
+    }
 
     render() {
 
@@ -55,7 +67,10 @@ export default class Todo extends Component {
                 <TodoForm description={this.state.description}
                     handleChange={this.handleChange}
                     handleAdd={this.handleAdd} />
-                <TodoList list={this.state.list} />
+                <TodoList list={this.state.list}
+                handleMarkAsDone={this.handleMarkAsDone}
+                handleMarkAsPending={this.handleMarkAsPending}
+                handleRemove={this.handleRemove} />
             </div>
         )
     }
--- src/todo/todoList.jsx
+++ src/todo/todoList.jsx
@@ -3,28 +3,33 @@ import IconButton from "../template/iconButton";
 
 const TodoList = props => {
   const renderRows = () => {
-      const list = props.list || []
-      return list.map(todo => (
-          <tr key={todo._id}>
-              <td>{todo.description}</td>
-              <td>
-                  <IconButton btnStyle='danger' icon='trash-o' onClick={() => props.handleRemove(todo)}/>
-              </td>
-          </tr>
-      )) 
+    const list = props.list || []
+    return list.map(todo => (
+      <tr key={todo._id}>
+        <td className={todo.done ? 'markeAsDone' : ''} >{todo.description}</td>
+        <td>
+          <IconButton btnStyle='sucess' icon='check' hide={todo.done}
+           onClick={() => props.handleMarkAsDone(todo)} />
+           <IconButton btnStyle='warning' icon='undo' hide={!todo.done}
+           onClick={() => props.handleRemove(todo)}/>
+          <IconButton btnStyle='danger' icon='trash-o' hide={!todo.done}
+          onClick={() => props.handleRemove(todo)} />
+        </td>
+      </tr>
+    ))
   }
   return (
-      <table className='table'>
-          <thead>
-              <tr>
-                  <th>Descrição</th>
-                  <th>Ações</th>
-              </tr>
-          </thead>
-          <tbody>
-              {renderRows()}
-          </tbody>
-      </table>
+    <table className='table'>
+      <thead>
+        <tr>
+          <th>Descrição</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        {renderRows()}
+      </tbody>
+    </table>
   )
 }
 
