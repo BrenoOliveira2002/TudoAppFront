30a4ed94aaa297f97cf2c03f02397c3736f4d076
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-07-01 08:54:29 -0300

    event add

--- src/template/iconButton.jsx
+++ src/template/iconButton.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import If from "./if";
+import React from 'react'
+import If from './if'
 
-export default props => (
-
-    <If teste={[props.hide]}>
-        <button className={'btn btn-' + props.style}
-            onClick={props.onClick}>
-            <i className={'fa fa-' + props.icon} > </i>
-        </button>
+const IconButton = props => (
+    <If test={!props.hide}>
+            <button className={'btn btn-'+ props.btnStyle} onClick={props.onClick}>
+                <i className={'fa fa-'+ props.icon}></i>
+            </button>
     </If>
-    )
+)
+
+export default IconButton
--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -4,15 +4,25 @@ import TodoForm from "./todoForm";
 import TodoList from "./todoList";
 
 export default class Todo extends Component {
+    
+    constructor(props) {
+
+        super(props)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleAdd() {
+
+        console.log('Add')
+    }
 
     render() {
 
         return (
             <div>
                 <Pageheader name='Tarefas' small='Cadastro'></Pageheader>
-                <TodoForm />
+                <TodoForm   handleAdd={this.handleAdd} />
                 <TodoList />
-
             </div>
         )
     }
